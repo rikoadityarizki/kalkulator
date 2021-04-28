7469eca30f3c9c631c5e15e45c3b3ce913c07f05
rikoadityarizki <rikoadityarizki07@gmail.com>
2021-04-28 11:32:58 -0700

    my first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:kalkulator/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+g
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blue,
+        accentColor: Colors.orange,
+      ),
+      home: Home(),
+    );
+  }
+}
